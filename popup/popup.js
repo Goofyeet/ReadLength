07e3530129cf1686b3bea7f59e44b40cb4161dcd
Austin <austinputland6464@gmail.com>
2023-03-29 11:05:01 -0300

    implement display time function

--- popup/popup.js
+++ popup/popup.js
@@ -7,18 +7,36 @@ function contentLoaded(){
     }
     
     //displays the time to read the web page
+    //param: length is the time in minutes : number
     function displayTime(length){
         let time = document.getElementById("time");
+
+        if(length >= 60){
+            //at least an hour to read
+            let hours = Math.floor(length / 60);
+            let minutes = length % 60;
+            length = hours +"h " + minutes + "m";
+        }
+        else if(length < 0){
+            //length is negative, something is wrong
+            length = "error";
+            console.error("The length is negative");
+        }
+        else if(length == 0){
+            //takes less than a minute to read
+            length = "< 1m";
+        }
+
         time.textContent += " " + length;
     }
 
     //calculates the time needed to read
-    //return: 
+    //params: speed is wpm : number, numWords is number of words: number
+    //return: the time in minutes : number
     function calculateTime(speed, numWords){
-        let lengthMinutes = numWords / speed;
-
-        console.log("Time to read: " + lengthMinutes);
+        let lengthMinutes = Math.floor(numWords / speed);
         
+        return lengthMinutes;
     }
     
     function getWpmInput(event){
@@ -31,6 +49,8 @@ function contentLoaded(){
 
     displayNumWords(10);
     calculateTime(238, 409);
+
+    displayTime(-5);
 }
 
 document.addEventListener("DOMContentLoaded", contentLoaded);
