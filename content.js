9935ffb7f11fe9e90d3e37bbfcf1d948de2d67f2
Austin <austinputland6464@gmail.com>
2023-03-29 11:04:00 -0300

    Add basic word counter function

--- content.js
+++ content.js
@@ -1,3 +1,25 @@
-let numText = document.querySelector("body").innerText;
+//counts the number of words in the visual text
+function countWords(text){
+    let count = 0;
+    let newWord = true;
 
-console.log(numText.length);
+    //loop through every character of visual text
+    for(i = 0; i < text.length; i++){
+        if(text[i] === ' '){
+            //console.log(i);
+            newWord = true;
+        }
+        else if(newWord){
+            count++;
+            newWord = false;
+        }
+    }
+
+    console.log(count);
+    return count;
+}
+
+let visualText = document.querySelector("body").innerText;
+
+console.log(visualText.length);
+countWords(visualText);
